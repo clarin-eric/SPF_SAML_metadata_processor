43a2cfb6cbfb08ee5a333e753c2088ca2e3b5174
Sander Maijers <S.N.Maijers@gmail.com>
2016-07-30 16:42:50 +0200

    Clean up
    * Use standard import rules as required by pre-commit.
    * Apply YAPF 0.11.0 style.
    * Shorten names and lines.
    * Rename main module to comply with PyLint naming rules.

--- setup.py
+++ setup.py
@@ -1,35 +1,25 @@
 #!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
-
-from platform import python_implementation
-from sys import version_info
 from setuptools import setup
 
 __author__ = 'Sander Maijers <sander@clarin.eu>'
 __version__ = '1.0.dev0'
 
-required_python_version = (3, 4,)
-python_implementation_str = python_implementation()
-
-if not (python_implementation_str == 'CPython' and (version_info.major, version_info.minor) == required_python_version):
-    raise RuntimeError('ERROR: running under unsupported {python_implementation:s} version '
-                       '{major_version:d}.{minor_version:d}. Please consult the documentation for supported platforms. '
-                       .format(python_implementation=python_implementation,
-                               major_version=version_info.major,
-                               minor_version=version_info.minor))
-
-install_requires = ['lxml>3.4,<3.5']  # TODO: version
+INSTALL_REQUIRES = ['lxml>3.4,<3.5']  # TODO: version
 
 setup(author=__author__,
       author_email=__author__,
-      classifiers=('Natural Language :: English', 'Programming Language :: Python',),
-      data_files=[('static', ['SPF_SAML_metadata_processor/static/remove_key_whitespace.xsl',
-                              'SPF_SAML_metadata_processor/static/remove_namespace_prefixes.xsl'])],
+      classifiers=('Natural Language :: English',
+                   'Programming Language :: Python',),
+      data_files=[('static', ['SPF_SAML_metadata_processor/static/'
+                              'remove_key_whitespace.xsl',
+                              'SPF_SAML_metadata_processor/static/'
+                              'remove_namespace_prefixes.xsl'])],
       # TODO: redundant?
-      description='Collects, filters, splits/aggregates, SAML metadata about CLARIN SPF SPs across identity federations'
-                  ' to assess its current state. Results are shown by the Centre Registry.',
+      description='Collects, filters, splits/aggregates, SAML metadata about '
+                  'CLARIN SPF SPs across identity federations to assess its '
+                  'current state. Results are shown by the Centre Registry.',
       include_package_data=True,
-      install_requires=install_requires,
+      install_requires=INSTALL_REQUIRES,
       license='GPLv3',
       name='SPF_SAML_metadata_processor',
       packages=['SPF_SAML_metadata_processor'],
